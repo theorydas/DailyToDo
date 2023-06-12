97c7bde1b82b6ba258d0c95c3138696d11cfa51c
KittenPirate <friesbi7porto@gmail.com>
2023-06-12 23:42:06 +0200

    Compactify DayListBanner

--- DailyTODO/ContentView.swift
+++ DailyTODO/ContentView.swift
@@ -39,6 +39,28 @@ struct DayListBanner: View {
                 .shadow(color: .black, radius: .pi)
                 
                 Spacer()
+    func GetCurretDay(DaysInFuture: Int = 0) -> String {
+        let DaysInFuture = Double (DaysInFuture * 60 * 60 * 24)
+        let date = Date(timeIntervalSinceNow: DaysInFuture)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        let dayInWeek = dateFormatter.string(from: date)
+        
+        return dayInWeek
+    }
+
+    func GetCurrentDate(DaysInFuture: Int = 0) -> String {
+        // Returns the DD Month YYYY format of the current date
+        let DaysInFuture = Double (DaysInFuture * 60 * 60 * 24)
+        let date = Date(timeIntervalSinceNow: DaysInFuture)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMMM yyyy"
+        let fullDate = dateFormatter.string(from: date)
+
+        return fullDate
+    }
+}
+
                 HStack{
                     Button(action: {
                         print("LEFT LOL")
