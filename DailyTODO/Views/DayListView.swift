1a2f605965dfdaf4cf61f64c0f8d213d03beb520
KittenPirate <friesbi7porto@gmail.com>
2023-06-16 15:12:14 +0200

    Remove ViewModels

---
+++ DailyTODO/Views/DayListView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct DayListView: View {
+    @StateObject var tasksInDay = TaskModel()
+    @State var day: Day = Day()
+    
+    var body: some View {
+        VStack {
+            DayListBanner(day: day)
+            CalendarBubbles()
+            TaskListView(tasksInDay: tasksInDay)
+        }
+        .frame(width: 298, height: 380)
+        .ignoresSafeArea()
+    }
+}
+
+struct TaskListView: View {
+    @StateObject var tasksInDay: TaskModel
+    
+    var body: some View {
+        List {
+            // tasksInDay.tasks have a name which can be repeated, ForEach should be given a unique id despite that.
+            ForEach (tasksInDay.tasks, id: \.self) { taskObject in
+                TaskTodoView(task: taskObject, tasksInDay: tasksInDay)
+            }
+        }
+        Spacer()
+        Button(action: {
+            tasksInDay.addTask(taskText: "New Task (\(tasksInDay.tasks.count))")
+        }) {
+            Text("Add new task")
+        }
+        .buttonStyle(PlainButtonStyle())
+        .padding(.bottom, -30)
+    }
+}
