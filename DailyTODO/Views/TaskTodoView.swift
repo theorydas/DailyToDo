b2e36f539cf6a72a894a74e133a05b59376788bc
KittenPirate <friesbi7porto@gmail.com>
2023-06-18 11:21:55 +0200

    Automatically focus first task

--- DailyTODO/Views/TaskTodoView.swift
+++ DailyTODO/Views/TaskTodoView.swift
@@ -1,9 +1,10 @@
 import SwiftUI
 
 struct TaskTodoView: View {
+    @FocusState private var isTextFieldFocused: Bool
     @StateObject var task: Task
     var tasksInDay: TaskModel
-
+    
     var body: some View {
         HStack{
             Image(systemName: task.state == 0 ? "circle" : "circle.fill")
@@ -13,6 +14,12 @@ struct TaskTodoView: View {
             if task.state != 1 { // We separete the two views here, because TextField does not support strikethrough.
                 TextField("...", text: $task.text, onCommit: {tasksInDay.addTask()} )
                     .lineLimit(1)
+                    .focused($isTextFieldFocused)
+                    .onAppear(perform: {
+                        guard tasksInDay.tasks.count == 0 else { isTextFieldFocused.toggle(); return }
+                    })
+                    .onHover { hovering in if hovering { NSCursor.iBeam.set() } else { NSCursor.arrow.set() }
+                    }
             } else {
                 Text(task.text)
                     .opacity(0.5)
